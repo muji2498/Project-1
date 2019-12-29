9b2231d97d965c4f7f0f92556d608450d00d18fc
Mujahid <muji2498@users.noreply.github.com>
2019-12-29 01:14:34 +0000

    Update 18:01:2018 12:14AM.py

--- project/18:01:2018 12:14AM.py
+++ project/18:01:2018 12:14AM.py
@@ -1,3 +1,22 @@
-def secretMsg = 'you have found the secret message';
-secretMsg();
+class messages():
+  
 
+# this function just print a secret message to the console
+  def secretMsg(self):
+    print("You have found the secret message!!")
+#------------------------------------------------------
+    
+
+# this function is to calculate bmi using meters and
+# kilograms
+  def bmi_calc_meters(self, weight, height):
+    bmi = weight / height ^ 2
+    return bmi
+#------------------------------------------------------
+  
+  
+# this function is to calculate bmi using feet and lbs
+  def bmi_calc_feet(self, weight, height):
+    bmi = 703 * weight / height ^ 2
+    return bmi
+#------------------------------------------------------
